9819d4cafa2ced32493a4afe1bc33a394f2d0a80
Tim Shim <timshim@gmail.com>
2018-08-14 10:17:09 +0800

    Fixed login box

--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,33 @@
 <template>
-  <section class="container box" style="max-width:350px;margin-top:20px;">
-    <h1 class="title is-4">Dashboard Login</h1>
-    <hr style="margin-top:-10px;"/>
-    <form v-on:submit.prevent="login">
-      <div class="field">
-        <input class="input" type="email" placeholder="Email" v-model="email">
-      </div>
-      <div class="field">
-        <input class="input" type="password" placeholder="Password" v-model="password">
-      </div>
-      <div class="field" v-if="showError">
-        <p class="help is-danger">{{ errorMessage }}</p>
-      </div>
-      <div class="field">
-        <p class="control" v-if="isLoading">
-          <button class="button is-info is-loading">
-            Login
-          </button>
-        </p>
-        <p class="control" v-else>
-          <button class="button is-info" @click="login">
-            Login
-          </button>
-        </p>
-      </div>
-    </form>
-  </section>
+  <div>
+    <div class="container box" style="max-width:350px;">
+      <h1 class="title is-4">Dashboard Login</h1>
+      <hr style="margin-top:-10px;"/>
+      <form v-on:submit.prevent="login">
+        <div class="field">
+          <input class="input" type="email" placeholder="Email" v-model="email">
+        </div>
+        <div class="field">
+          <input class="input" type="password" placeholder="Password" v-model="password">
+        </div>
+        <div class="field" v-if="showError">
+          <p class="help is-danger">{{ errorMessage }}</p>
+        </div>
+        <div class="field">
+          <p class="control" v-if="isLoading">
+            <button class="button is-info is-loading">
+              Login
+            </button>
+          </p>
+          <p class="control" v-else>
+            <button class="button is-info" @click="login">
+              Login
+            </button>
+          </p>
+        </div>
+      </form>
+    </div>
+  </div>
 </template>
 
 <script>
