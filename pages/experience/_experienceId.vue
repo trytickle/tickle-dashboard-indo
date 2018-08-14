4de1ed7be177e2974e9eb0621d2ed60ee8be2fea
Tim Shim <timshim@gmail.com>
2018-08-14 17:17:28 +0800

    Added experience with isEditMode

---
+++ pages/experience/_experienceId.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1 class="title">{{experienceId}}</h1>
+    <h1 class="subtitle" v-if="isEditMode">Edit Mode</h1>
+  </div>
+</template>
+
+<script>
+import { auth, db } from '~/plugins/firebase'
+
+export default {
+  data() {
+    return {
+      experienceId: this.$route.params.experienceId
+    }
+  },
+  computed: {
+    isEditMode() {
+      if (this.$route.query.isEditMode) {
+        return this.$route.query.isEditMode
+      } else {
+        return false
+      }
+    }
+  },
+  created() {
+    auth.onAuthStateChanged(user => {
+      if (!user) {
+        this.$router.push('/')
+      }
+    })
+  }
+}
+</script>
